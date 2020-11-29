49bcfa721baa778e7950996f632bcbefdc8bb1ca
Gal Dolber <gal.dolber@gmail.com>
2020-11-29 17:02:06 -0300

    Use Base64

--- src/org/httpkit/encode.clj
+++ src/org/httpkit/encode.clj
@@ -1,9 +1,5 @@
-(ns org.httpkit.encode)
+(ns org.httpkit.encode
+  (:import [java.util Base64]))
 
 (defmacro base64-encode [bs]
-  (if (try (import 'javax.xml.bind.DatatypeConverter)
-           (catch ClassNotFoundException _))
-    `(javax.xml.bind.DatatypeConverter/printBase64Binary ~bs)
-    (do
-      (import 'java.util.Base64)
-      `(.encodeToString (java.util.Base64/getEncoder) ~bs))))
+  `(.encodeToString (Base64/getEncoder) ~bs))
